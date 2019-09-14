f0df9cfac85a088df67f79f5e21248ec8bb91d20
brankohbk <brankoh@gmail.com>
2019-09-14 00:07:51 -0300

    First commit Ver 0.1 BETA

---
+++ js/app.js
@@ -0,0 +1,25 @@
+let nysl = new Vue({
+  el: '#app',
+  data: {
+    fecha: "",
+    consorcio: "",
+    uf: "",
+    depto: "",
+    propietario: "",
+    deuda: "",
+    ultimaLiquidacion: "",
+    vencimiento: "",
+    expensaNeta: "",
+    aseguradora: "",
+    poliza: "",
+    vigenciaSeguro: "",
+    fondoCaja: "",
+    porcentual: "",
+    juicio: "",
+    aysa: "",
+    abl: "",
+
+  },
+  methods: {},
+  computed: {}
+});
